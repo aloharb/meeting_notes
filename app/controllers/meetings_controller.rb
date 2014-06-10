bbae9252f3c5eca19d31a81bb0da7523f9821e93
fergaloy <fglhawaii@yahoo.com>
2014-06-10 12:56:53 -1000

    First stab at the page used to record meeting notes.

--- app/controllers/meetings_controller.rb
+++ app/controllers/meetings_controller.rb
@@ -1,5 +1,5 @@
 class MeetingsController < ApplicationController
-  before_action :set_meeting, only: [:show, :edit, :update, :destroy]
+  before_action :set_meeting, only: [:show, :edit, :update, :record, :destroy]
 
   # GET /meetings
   # GET /meetings.json
@@ -19,6 +19,10 @@ class MeetingsController < ApplicationController
 
   # GET /meetings/1/edit
   def edit
+  end
+
+  # GET /meetings/1/record
+  def record
   end
 
   # POST /meetings
